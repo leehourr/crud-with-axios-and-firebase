ef549b78892fc0473e5833811d9e4a5fb61fe50b
Lee Hour <lhour999@gmail.com>
2022-12-20 18:27:36 +0700

    same bug fixed

--- src/components/dog/Dog.js
+++ src/components/dog/Dog.js
@@ -1,61 +1,62 @@
-import {  useFrame, useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useGLTF, PresentationControls } from "@react-three/drei";
 import { a, useSpring } from "@react-spring/three";
 import { useState, useRef, useMemo } from "react";
 import { useDrag } from "react-use-gesture";
 import * as THREE from "three";
 
-const Box = ({ position }) => {
-  const [isSlowDown, setIsSlowDown] = useState(false);
-  const { scene } = useGLTF("/dog.glb");
-  const ref = useRef();
-  let spin;
-  // console.log(isSlowDown);
-
-  setTimeout(() => {
-    setIsSlowDown(true);
-    // console.log(isSlowDown);
-  }, [800]);
-  spin = isSlowDown ? 0.01 : 0.4;
-  useFrame(() => (ref.current.rotation.y += spin));
-  return (
-    <mesh position={position} ref={ref}>
-      <primitive object={scene} scale={0.4} />
-    </mesh>
-  );
-};
-
-const Inspector = ({ responsiveness = 20, children }) => {
-  const { size } = useThree();
-  const euler = useMemo(() => new THREE.Euler(), []);
-  const [spring, set] = useSpring(() => ({
-    rotation: [0, 0, 0],
-  }));
-  const bind = useDrag(({ delta: [dx, dy] }) => {
-    euler.y += (dx / size.width) * responsiveness;
-    // euler.x += (dy / size.width) * responsiveness;
-    //euler.x = THREE.MathUtils.clamp(euler.x, -Math.PI / 2, Math.PI / 2);
-    set({ rotation: euler.toArray().slice(0, 3) });
-  });
-  return (
-    <a.group {...bind()} {...spring}>
-      {children}
-    </a.group>
-  );
-};
-
-const Scene = ({ speed }) => {
-  return (
-    <>
-      <color attach="background" args={["black"]} />
-      <Inspector>
-        <Box speed={speed} />
-      </Inspector>
-    </>
-  );
-};
-
 export const Dog = () => {
+
+  const Box = ({ position }) => {
+    const [isSlowDown, setIsSlowDown] = useState(false);
+    const { scene } = useGLTF("/dog.glb");
+    const ref = useRef();
+    let spin;
+    // console.log(isSlowDown);
+
+    setTimeout(() => {
+      setIsSlowDown(true);
+      // console.log(isSlowDown);
+    }, [800]);
+    spin = isSlowDown ? 0.01 : 0.4;
+    useFrame(() => (ref.current.rotation.y += spin));
+    return (
+      <mesh position={position} ref={ref}>
+        <primitive object={scene} scale={0.4} />
+      </mesh>
+    );
+  };
+
+  const Inspector = ({ responsiveness = 20, children }) => {
+    const { size } = useThree();
+    const euler = useMemo(() => new THREE.Euler(), []);
+    const [spring, set] = useSpring(() => ({
+      rotation: [0, 0, 0],
+    }));
+    const bind = useDrag(({ delta: [dx, dy] }) => {
+      euler.y += (dx / size.width) * responsiveness;
+      // euler.x += (dy / size.width) * responsiveness;
+      //euler.x = THREE.MathUtils.clamp(euler.x, -Math.PI / 2, Math.PI / 2);
+      set({ rotation: euler.toArray().slice(0, 3) });
+    });
+    return (
+      <a.group {...bind()} {...spring}>
+        {children}
+      </a.group>
+    );
+  };
+
+  const Scene = ({ speed }) => {
+    return (
+      <>
+        <color attach="background" args={["black"]} />
+        <Inspector>
+          <Box speed={speed} />
+        </Inspector>
+      </>
+    );
+  };
+
   return (
     <>
       <ambientLight intensity={0.7} />
