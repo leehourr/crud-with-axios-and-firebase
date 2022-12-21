e58f657f5282ce51601a4a88abb4ef4f85a9811a
Lee Hour <lhour999@gmail.com>
2022-12-21 14:30:30 +0700

    ...

--- src/pages/AllContacts.js
+++ src/pages/AllContacts.js
@@ -79,7 +79,7 @@ export const AllContacts = () => {
           CONTACT
         </h2>
       </div>
-      <div className="flex max-w-lg w-full flex-row justify-between space-x-14">
+      <div className="flex max-w-lg w-full flex-row items-center justify-between space-x-14">
         <input
           type="text"
           id="search"
